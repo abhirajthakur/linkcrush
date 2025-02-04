a1091fc5a1854333cacd122ce4a42dd273fe5bae
abhirajthakur <abhirajthakur124@gmail.com>
2025-02-04 04:53:44 +0000

    Update middleware to handle cors and update the URL regex pattern to verify valid URL

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -7,6 +7,15 @@ import (
 func EnableCors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		// Preflight requests
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+
 		next.ServeHTTP(w, r)
 	})
 }
